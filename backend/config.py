ea3902e8e5bd08d0f036beb986f09f46cbcb5a65
tonan <126637328+tonantr@users.noreply.github.com>
2023-10-09 23:57:05 -0400

    add app.py, config.py, models.py

---
+++ backend/config.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
+
+metadata = MetaData()
+
+db.init_app(app)
---
+++ backend/models.py
@@ -0,0 +1,67 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+
+from config import db, bcrypt
+
+
+class User(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+    family_id = db.Column(db.Integer, db.ForeignKey("family.id"), nullable=False)
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+        self.name = name
+        self.email = email
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username}, name={self.name}, email={self.email})"
+
+
+class Family(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    users = db.relationship("User", backref="family", lazy=True)
+
+    def __repr__(self):
+        return f"Family(id={self.id}, name={self.name})"
+
+
+class Product(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    brand = db.Column(db.String(100))
+    model = db.Column(db.String(100))
+    serial_number = db.Column(db.String(100))
+    purchase_date = db.Column(db.Date)
+    warranty_expiration_date = db.Column(db.Date)
+
+    def __repr__(self):
+        return (
+            f"Product("
+            f"id={self.id}, "
+            f"name={self.name}, "
+            f"brand={self.brand}, "
+            f"model={self.model}, "
+            f"serial_number={self.serial_number}, "
+            f"purchase_date={self.purchase_date}, "
+            f"warranty_expiration_date={self.warranty_expiration_date}"
+            f")"
+        )
+
+
+class FamilyProductAssociation(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    family_id = db.Column(db.Integer, db.ForeignKey("family.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
+    family = db.relationship("Family", backref="product_associations")
+    product = db.relationship("Product", backref="family_associations")
