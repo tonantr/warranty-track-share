af0114e0aec5d608986603e52eeebb6502be765a
tonan <126637328+tonantr@users.noreply.github.com>
2023-10-10 00:02:07 -0400

    add authcontext.js to frontend

---
+++ frontend/src/components/AuthContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null)
+
+    const login = (userDate) => {
+        setUser(userDate)
+    };
+
+    const logout = () => {
+        setUser(null)
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+}
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
