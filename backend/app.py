70442818577075798760a8466b2cefbefee920d4
tonan <126637328+tonantr@users.noreply.github.com>
2023-10-10 22:32:07 -0400

    add the login class

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,23 @@ class Index(Resource):
     def get(self):
         return {"message": "Welcome!"}
 
+class Login(Resource):
+    def post(self):
+        json_data = request.get_json()
+        username = json_data.get("username")
+        password = json_data.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            session["user_id"] = user.id
+            return {"message": "Login successful", "username": user.username}, 200
+        else:
+            return {"message": "Invalid username or password"}, 401
+
 
 api.add_resource(Index, "/")
+api.add_resource(Login, "/login", endpoint="login")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
